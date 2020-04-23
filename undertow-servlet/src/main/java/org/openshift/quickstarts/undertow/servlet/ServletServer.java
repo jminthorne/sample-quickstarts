19c6c339fce985a9171a5fe246b9e33b90f67e18
josh@lattapartners.com <josh.minthorne@axcelinno.io>
2020-04-23 18:02:10 +0000

    updated the message  to show the change to everyone

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello Everyone")
+                                    .addInitParam("message", "Hello Everyone! Welcome to this demo of me making a change!")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")

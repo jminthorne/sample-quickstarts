66ae272a53a4aac7640493d123b1fb2d4e3b7d70
josh@lattapartners.com <josh.minthorne@axcelinno.io>
2020-05-06 15:10:17 +0000

    reset message

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello AMA! I made a change to code and deployed!")
+                                    .addInitParam("message", "Hello World!")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
